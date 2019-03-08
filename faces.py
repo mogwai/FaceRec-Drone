e10f5006e59e4396bafacdf83f5e52b11cc66429
Harry <himion0@gmail.com>
2019-03-08 13:58:15 +0000

    Added model

--- faces.py
+++ faces.py
@@ -1,7 +1,13 @@
 import sys
 import dlib
+import requests
 
 cnn_face_detector = dlib.cnn_face_detection_model_v1('./face_model.dat')
+
+model_url = 'http://dlib.net/files/mmod_human_face_detector.dat.bz2'
+
+def download():
+  himion0
 
 def detect(img):
     dets = cnn_face_detector(img, 1)
