faadc50163ae1a2fcae0fbba200ec49656e44738
Harry <himion0@gmail.com>
2019-03-12 19:41:22 +0000

    Simpler face detector

--- faces.py
+++ faces.py
@@ -2,15 +2,14 @@ import sys
 import dlib
 import requests
 
-cnn_face_detector = dlib.cnn_face_detection_model_v1('./face_model.dat')
 
-model_url = 'http://dlib.net/files/mmod_human_face_detector.dat.bz2'
-
-def download():
-  himion0
+cnn_face_detector = dlib.get_frontal_face_detector()
 
 def detect(img):
     dets = cnn_face_detector(img, 1)
-    rects = dlib.rectangles()
-    rects.extend([d.rect for d in dets])
-    return rects
+    arr = []
+    for i in range(len(dets)):
+        c = dets[i]
+        tr = c.tl_corner()
+        arr.append((tr.x, tr.y, c.width(), c.height()))
+    return arr
