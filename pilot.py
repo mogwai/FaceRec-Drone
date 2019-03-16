e90ee8ab891f6cba958a01aa20d67bbdfa6212d3
Harry Blum <himion0@gmail.com>
2019-03-16 11:23:04 +0000

    Look for biggest face

--- pilot.py
+++ pilot.py
@@ -19,52 +19,66 @@ class Pilot:
 
     def on_frame(self, frame):
         # These are our center dimensions
-        cWidth = F_WIDTH // 2
-        cHeight = F_HEIGHT // 2
-
         frameRet, im_height = resize_image_arr(frame, CROP_WIDTH)
         faces = detect(frameRet)
-
-        wR = F_WIDTH / CROP_WIDTH
-        hR = F_HEIGHT / im_height
 
         # Record that we've seen a face
         if len(faces) > 0:
             self.last_face_seen = time.time()
 
-        for (x, y, w, h) in faces:
-
-            # Put coordinates back into original scale
-            x = int(x * wR)
-            w = int(w * wR)
-            y = int(y * hR)
-            h = int(h * hR)
-
-            # for turning
-            yawR = (x - cWidth) / cWidth
-            self.tello.yaw_velocity = int(
-                50 * yawR)
-
-            # We want the height to be relative to our ideal position
-            heightR = - (y - (cHeight-100)) / cHeight
-            self.tello.up_down_velocity = int(40 * heightR)
-
-            # We want the width to be our ideal width (Hacky depth perception)
-            forwardR = (FBOX_Z - w) / FBOX_Z
-            self.tello.forward_backward_velocity = int(50 * forwardR)
-
-            # Draw box on UI Frame
-            self.ui.draw_box(frame, x, y, x+w, y+h, cWidth, cHeight)
-
         if len(faces) < 1:
             if self.last_face_seen < time.time() - (1000 * TIME_TIL_SEARCH):
-                self.tello.yaw_velocity = S*3
-                height = int(self.tello.get_height().replace(
-                    'ok', '').replace('dm', ''))
-                if height < 10:
-                    self.tello.up_down_velocity = S
+                self.start_search()
             else:
                 self.tello.yaw_velocity = 0
                 self.tello.up_down_velocity = 0
 
             self.tello.forward_backward_velocity = 0
+            return
+
+        biggest = None
+        maxArea = 0
+
+        for i in range(len(faces)):
+            (_, __, w, h) = faces[i]
+            if w * h > maxArea:
+                maxArea = w * h
+                biggest = faces[i]
+
+        self.move_to_face(*biggest, im_height)
+
+        # Draw boxes on UI Frame
+        for (x, y, w, h) in faces:
+            self.ui.draw_box(frame, x, y, x+w, y+h,
+                             F_WIDTH // 2, F_HEIGHT // 2)
+
+    def move_to_face(self, x, y, w, h, im_height):
+        cWidth = F_WIDTH // 2
+        cHeight = F_HEIGHT // 2
+        wR = F_WIDTH / CROP_WIDTH
+        hR = F_HEIGHT / im_height
+
+        # Put coordinates back into original scale
+        x = int(x * wR)
+        w = int(w * wR)
+        y = int(y * hR)
+
+        # Turning
+        yawR = (x - cWidth) / cWidth
+        self.tello.yaw_velocity = int(
+            50 * yawR)
+
+        # We want the height to be relative to our ideal position
+        heightR = - (y - (cHeight-100)) / cHeight
+        self.tello.up_down_velocity = int(40 * heightR)
+
+        # We want the width to be our ideal width (Hacky depth perception)
+        forwardR = (FBOX_Z - w) / FBOX_Z
+        self.tello.forward_backward_velocity = int(50 * forwardR)
+
+    def start_search(self):
+        self.tello.yaw_velocity = 50*3
+        height = int(self.tello.get_height().replace(
+            'ok', '').replace('dm', ''))
+        if height < 10:
+            self.tello.up_down_velocity = 40
